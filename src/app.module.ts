2796c9491b433bd684a01c8f76d9412e279d2f2d
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-17 00:00:08 +0700

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nest_graphql',
+      entities: ['dist/**/*.model.js'],
+      synchronize: true,
+    }),
+  ]
+})
+export class AppModule {}
