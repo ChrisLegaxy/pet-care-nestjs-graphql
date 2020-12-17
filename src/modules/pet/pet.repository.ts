5957ab29d9d5104e565c34c938a0ba705a92fd4f
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-17 22:06:30 +0700

    feat(module): add complete pet repository

---
+++ src/modules/pet/pet.repository.ts
@@ -0,0 +1,35 @@
+/**
+ * @file - Pet's Repository
+ * @description - Typeorm Repository
+ * @copyright - CPC
+ * @version - 1.0.0
+ * @author - Vansen Hengmeanrith aka Chris - chris.vhsmr@gmail.com
+ * @since - 17 Dec 2020
+ */
+
+/**
+ * * Package Imports
+ */
+import { EntityRepository, Repository } from 'typeorm';
+
+/**
+ * * Pet & Local Imports
+ */
+import { Pet } from './pet.model';
+
+/**
+ * @class PetRepository
+ */
+@EntityRepository(Pet)
+export class PetRepository extends Repository<Pet> {
+  /**
+   * @description - first create the instance then save to the database
+   *
+   * @function createAndSave
+   * @param pet
+   * @returns Promise<Pet>
+   */
+  async createAndSave(pet: Partial<Pet>): Promise<Pet> {
+    return await this.save(this.create(pet));
+  }
+}
