783e484814dae70f50c5a4b21bec3dae928e9448
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-17 22:06:51 +0700

    feat(module): add pet controller

---
+++ src/modules/pet/pet.controller.ts
@@ -0,0 +1,19 @@
+/**
+ * @file - Pet's Controller
+ * @description - Nest JS Controller
+ * @copyright - CPC
+ * @version - 1.0.0
+ * @author - Vansen Hengmeanrith aka Chris - chris.vhsmr@gmail.com
+ * @since - 17 Dec 2020
+ */
+
+/**
+ * * Nest JS & Package Imports
+ */
+import { Controller } from '@nestjs/common';
+
+/**
+ * @class PetController
+ */
+@Controller('pet')
+export class PetController {}
