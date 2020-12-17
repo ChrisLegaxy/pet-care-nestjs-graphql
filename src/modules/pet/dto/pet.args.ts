d707e4d4f695b48c7bc1fe7cedfa467040db4d81
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-17 22:07:39 +0700

    feat(module): add base pet args type

---
+++ src/modules/pet/dto/pet.args.ts
@@ -0,0 +1,16 @@
+/**
+ * @file - Pet's GraphQL Args Type
+ * @description - Args type for GraphQL query & mutation
+ * @copyright - CPC
+ * @version - 1.0.0
+ * @author - Vansen Hengmeanrith aka Chris - chris.vhsmr@gmail.com
+ * @since - 17 Dec 2020
+ */
+
+/**
+ * * Nest JS & Package Imports
+ */
+import { ArgsType } from '@nestjs/graphql';
+
+@ArgsType()
+export class PetArgs {}
