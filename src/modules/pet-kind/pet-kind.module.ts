2fb4b2c2e70bcd7efb7efc0357229881b5ec5bbe
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-18 22:52:48 +0700

    Create pet-kind.module.ts

---
+++ src/modules/pet-kind/pet-kind.module.ts
@@ -0,0 +1,18 @@
+/**
+ * @file - PetKind's Module
+ * @description - Nest JS Module
+ * @copyright - CPC
+ * @version - 1.0.0
+ * @author - Vansen Hengmeanrith aka Chris - chris.vhsmr@gmail.com
+ * @since - 17 Dec 2020
+ */
+
+/**
+ * * Nest JS Module Imports
+ */
+import { Module } from '@nestjs/common';
+
+
+@Module({
+})
+export class PetKindModule {}
