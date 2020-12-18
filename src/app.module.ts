261ff05b431971be1f8bea8be923eda2ef9faa2e
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-18 23:00:52 +0700

    Update app.module.ts
    add comments & import PetKind module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
+/**
+ * @file - Root Application Module
+ * @description - Main root module that import all the modules & more
+ * @copyright - CPC
+ * @version - 1.0.0
+ * @author - Vansen Hengmeanrith aka Chris - chris.vhsmr@gmail.com
+ * @since - 17 Dec 2020
+ */
+
+ /**
+  * * Nest JS Module Imports
+  */
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
-import { PetModule } from './modules/pet/pet.module';
 
+/**
+ * * Application Module Imports
+ */
+import { PetModule } from './modules/pet/pet.module';
+import { PetKindModule } from './modules/pet-kind/pet-kind.module';
+
+/**
+ * * Other Imports
+ */
+import { join } from 'path';
+
+/**
+ * @module AppModule
+ */
 @Module({
   imports: [
     GraphQLModule.forRoot({
@@ -21,7 +45,8 @@ import { PetModule } from './modules/pet/pet.module';
       entities: ['dist/**/*.model.js'],
       synchronize: true
     }),
-    PetModule
+    PetModule,
+    PetKindModule
   ]
 })
 export class AppModule {}
