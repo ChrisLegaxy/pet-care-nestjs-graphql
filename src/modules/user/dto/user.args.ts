b0ca6d05444dc4ce2bcfa4f5255561bd976bb303
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-19 21:51:17 +0700

    Create user.args.ts

---
+++ src/modules/user/dto/user.args.ts
@@ -0,0 +1,16 @@
+/**
+ * @file - User's GraphQL Args Type
+ * @description - Args type for GraphQL query & mutation
+ * @copyright - CPC
+ * @version - 1.0.0
+ * @author - Vansen Hengmeanrith aka Chris - chris.vhsmr@gmail.com
+ * @since - 19 Dec 2020
+ */
+
+/**
+ * * Nest JS Module Imports
+ */
+import { ArgsType } from '@nestjs/graphql';
+
+@ArgsType()
+export class UserArgs {}
