6d5508ab24e9bac92559abf7e529a67f578e2444
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-20 03:04:44 +0700

    Update user.resolver.ts
    (Re add) Add complete UserResolver with all necessary query & mutation. And make changes to Resolve Field (pets) to return null if user.id not presented (meaning deleted)

--- src/modules/user/user.resolver.ts
+++ src/modules/user/user.resolver.ts
@@ -1,2 +1,119 @@
+/**
+ * @file - User's Resolver
+ * @description - GraphQL Resolver
+ * @copyright - CPC
+ * @version - 1.0.0
+ * @author - Vansen Hengmeanrith aka Chris - chris.vhsmr@gmail.com
+ * @since - 10 Dec 2020
+ */
+
+/**
+ * * Nest JS Module Imports
+ */
+import { Injectable } from '@nestjs/common';
+import { Resolver, Query, Args, Mutation, ResolveField, Parent } from '@nestjs/graphql';
+
+/**
+ * * Node Packages Imports
+ */
+import { plainToClass } from 'class-transformer';
+
+/**
+ * * Internal Imports
+ */
+import { User } from './user.model';
+import { UserService } from './user.service';
+
+import { UserType } from './dto/user.type';
+import { CreateUserInput, UpdateUserInput } from './dto/user.input';
+import { PetType } from '../pet/dto/pet.type';
+import { PetService } from '../pet/pet.service';
+import { Pet } from '../pet/pet.model';
+
+/**
+ * @class PetKindResolver
+ */
+@Resolver(_of => UserType)
+@Injectable()
+export class UserResolver {
+  constructor(private userService: UserService,
+              private petService: PetService) {}
+
+  /**
+   * @description - query that return one user record by id
+   *
+   * @function user
+   * @param id
+   * @returns Promise<User>
+   */
+  @Query(_returns => UserType)
+  async user(@Args('id', { type: () => String }) id: string): Promise<User> {
+    return this.userService.findByIdOrFail(id);
+  }
+
+  /**
+   * @description - query that return all user records
+   *
+   * @function users
+   * @param none
+   * @returns Promise<User[]>
+   */
+  @Query(_returns => [UserType])
+  async users(): Promise<User[]> {
+    return this.userService.find();
+  }
+
+  /**
+   * @description - mutation that create a user record
+   *
+   * @function createUser
+   * @param user - type of CreateUserInput
+   * @returns Promise<User>
+   */
+  @Mutation(_returns => UserType)
+  async createUser(@Args('user') user: CreateUserInput): Promise<User> {
+    /**
+     * ! must cast with plainToClass because user input will return as [Object: null prototype]
+     */
+    return await this.userService.create(plainToClass(CreateUserInput, user));
+  }
+
+  /**
+   * @description - mutation that update a user record
+   *
+   * @function updateUser
+   * @param user - type of UpdateUserInput
+   * @returns Promise<User>
+   */
+  @Mutation(_returns => UserType)
+  async updateUser(
+    @Args('id', { type: () => String }) id: string,
+    @Args('user') user: UpdateUserInput
+  ): Promise<User> {
+    /**
+     * ! must cast with plainToClass because user input will return as [Object: null prototype]
+     */
+    return await this.userService.update(id, plainToClass(UpdateUserInput, user));
+  }
+
+  /**
+   * @description - mutation that delete a user record
+   *
+   * @function deleteUser
+   * @param user - type of UpdateUserInput
+   * @returns Promise<User>
+   */
+  @Mutation(_returns => UserType)
+  async deleteUser(
+    @Args('id', { type: () => String }) id: string
+  ): Promise<User> {
+    return await this.userService.delete(id);
+  }
+
+  @ResolveField(_returns => [PetType])
+  async pets(@Parent() user: User): Promise<Pet[]> {
     if (!user.id) { return null; }
 
+    return await this.petService.findByUserId(user.id);
+  }
+}
