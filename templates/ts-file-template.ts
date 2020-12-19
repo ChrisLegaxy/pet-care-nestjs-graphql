b5cbab9a290a2e5d84b2f9834a354f9028a3d57a
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-19 11:01:41 +0700

    Create ts-file-template.ts
    General file format

---
+++ templates/ts-file-template.ts
@@ -0,0 +1,24 @@
+/**
+ * @file - 
+ * @description - 
+ * @copyright - CPC
+ * @version - 1.0.0
+ * @author - Vansen Hengmeanrith aka Chris - chris.vhsmr@gmail.com
+ * @since - 
+ */
+
+/**
+ * * Nest JS Module Imports
+ */
+
+/**
+ * * Node Packages Imports
+ */
+
+/**
+ * * External Imports
+ */
+
+/**
+ * * Internal Imports
+ */
