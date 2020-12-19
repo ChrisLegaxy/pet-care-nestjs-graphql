dd715b5aba1dc48b17af6bbef5df1f1ba52c0981
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-19 11:18:14 +0700

    Update user.module.ts
    Add UserRepository

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,4 +1,30 @@
+/**
+ * @file - User's Module
+ * @description - Nest JS Module
+ * @copyright - CPC
+ * @version - 1.0.0
+ * @author - Vansen Hengmeanrith aka Chris - chris.vhsmr@gmail.com
+ * @since - 17 Dec 2020
+ */
+
+/**
+ * * Nest JS Module Imports
+ */
 import { Module } from '@nestjs/common';
 
-@Module({})
+/**
+ * * Node Packages Imports
+ */
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+/**
+ * * Internal Imports
+ */
+import { UserRepository } from './user.repository';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([UserRepository])
+  ]
+})
 export class UserModule {}
