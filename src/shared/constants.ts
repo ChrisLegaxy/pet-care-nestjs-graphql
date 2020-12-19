5e22653bb1aabe010b789aa2b3eee0b0b3a4c8a9
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-19 11:10:55 +0700

    Create constants.ts
    Add Gender in

---
+++ src/shared/constants.ts
@@ -0,0 +1,13 @@
+/**
+ * @file - Constants/Enum
+ * @description - Contains all constants within the application
+ * @copyright - CPC
+ * @version - 1.0.0
+ * @author - Vansen Hengmeanrith aka Chris - chris.vhsmr@gmail.com
+ * @since - 17 Dec 2020
+ */
+
+export enum Gender {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE'
+}
