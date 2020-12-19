0d656ffda0afda9087c93842aa2d8eac8d57cccf
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-20 02:59:20 +0700

    Update user.input.ts
    Add complete CreateUserInput & UpdateUserInput

--- src/modules/user/dto/user.input.ts
+++ src/modules/user/dto/user.input.ts
@@ -1,19 +1,104 @@
 /**
- * @file - User's GraphQL Args Type
- * @description - Args type for GraphQL query & mutation
+ * @file - User's GraphQL Input Type
+ * @description - Input type for GraphQL query & mutation
  * @copyright - CPC
  * @version - 1.0.0
  * @author - Vansen Hengmeanrith aka Chris - chris.vhsmr@gmail.com
- * @since - 17 Dec 2020
+ * @since - 19 Dec 2020
  */
 
 /**
  * * Nest JS Module Imports
  */
-import { InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
+
+/**
+ * * Node Packages Imports
+ */
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+/**
+ * * External Imports
+ */
+import { Gender, Role } from '@/shared/constants';
+
+/**
+ * @class CreateUserInput
+ * @description - For creating user via GraphQL mutation only (does not inlcude social sign in)
+ */
+@InputType()
+export class CreateUserInput {
+  /**
+   * * Base
+   */
+  @Field(_type => String)
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @Field(_type => String)
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @Field(_type => String)
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @Field(_type => String)
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @Field(_type => Gender)
+  @IsEnum(Gender)
+  @IsNotEmpty()
+  gender: Gender;
+
+  @Field(_type => Role, { nullable: true } )
+  role?: Role;
+
+  @Field(_type => String, { nullable: true })
+  phoneNumber?: string;
+}
 
 @InputType()
-export class CreateUserInput {}
-
+/**
+ * @class UpdateUserInput
+ */
 @InputType()
-export class UpdateUserInput {}
+export class UpdateUserInput {
+  /**
+   * * Base
+   */
+  @Field(_type => String, { nullable: true })
+  firstName?: string;
+
+  @Field(_type => String, { nullable: true })
+  lastName?: string;
+
+  @Field(_type => String, { nullable: true })
+  email?: string;
+
+  @Field(_type => String, { nullable: true })
+  password?: string;
+
+  @Field(_type => String, { nullable: true })
+  gender?: Gender;
+
+  @Field(_type => Role, { nullable: true })
+  role?: Role;
+
+  @Field(_type => String, { nullable: true })
+  phoneNumber?: string;
+
+  /**
+   * * Social Sign In
+   */
+  @Field(_type => String, { nullable: true })
+  google?: string;
+
+  @Field(_type => String, { nullable: true })
+  facebook?: string;
+}
