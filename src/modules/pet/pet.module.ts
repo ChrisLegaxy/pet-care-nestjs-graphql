429f3fdfd9dacd51dc8641b9bde6c6ecb2484325
Chris Legaxy <chris_legaxy@yahoo.com>
2020-12-19 10:51:02 +0700

    Update pet.module.ts
    Import PetKindModule & export PetService

--- src/modules/pet/pet.module.ts
+++ src/modules/pet/pet.module.ts
@@ -20,15 +20,19 @@ import { PetController } from './pet.controller';
 import { PetService } from './pet.service';
 import { PetRepository } from './pet.repository';
 import { PetResolver } from './pet.resolver';
+import { PetKindService } from '../pet-kind/pet-kind.service';
+import { PetKindModule } from '../pet-kind/pet-kind.module';
 
 /**
  * @module Pet
  */
 @Module({
   imports: [
-    TypeOrmModule.forFeature([PetRepository])
+    TypeOrmModule.forFeature([PetRepository]),
+    PetKindModule
   ],
   providers: [PetService, PetResolver],
-  controllers: [PetController]
+  controllers: [PetController],
+  exports: [PetService]
 })
 export class PetModule {}
